9df0476009d437bb9914e77b8f07e5f24794e24d
Luis-Martinez1 <Luis-Martinez1>
2024-04-18 16:25:07 +0000

    added comments

--- 1.5A_Views.sql
+++ 1.5A_Views.sql
@@ -1,4 +1,10 @@
 -- Databricks notebook source
+--The code creates a table called smartphones_lm if it does not already exist. The table has four columns: id, name, brand, and year.
+
+--The code then inserts ten rows of data into the smartphones_lm table using the INSERT INTO statement. Each row represents a smartphone and includes values for the id, name, brand, and year columns.
+
+--After running this code, the smartphones_lm table will exist and contain ten rows of smartphone data.
+
 CREATE TABLE IF NOT EXISTS smartphones_lm
 (id INT, name STRING, brand STRING, year INT);
 
@@ -20,6 +26,8 @@ SHOW TABLES
 
 -- COMMAND ----------
 
+--The code creates a view called "view_apple_phones" by selecting all data from the "smartphones_lm" table where the brand is 'Apple'. This view will only display the rows where the brand is 'Apple' and can be used to retrieve this filtered data easily in future queries.
+
 CREATE VIEW view_apple_phones
 AS  SELECT * 
     FROM smartphones_lm
@@ -35,6 +43,8 @@ SHOW TABLES;
 
 -- COMMAND ----------
 
+--This code creates a temporary view called "temp_view_phones_brands" that contains distinct values of the "brand" column from the "smartphones" table. It then selects all rows from the temporary view, displaying the distinct brands of smartphones.
+
 CREATE TEMP VIEW temp_view_phones_brands
 AS  SELECT DISTINCT brand
     FROM smartphones;
@@ -47,6 +57,8 @@ SHOW TABLES;
 
 -- COMMAND ----------
 
+-- This code creates a global temporary view named "global_temp_view_latest_phones". The view is created based on the "smartphones" table. It filters the data to only include smartphones with a year greater than 2020. The result is then ordered in descending order by year. The global temporary view can be accessed and used by any session but is automatically dropped at the end of the session.
+
 CREATE GLOBAL TEMP VIEW global_temp_view_latest_phones
 AS SELECT * FROM smartphones
     WHERE year > 2020
@@ -54,6 +66,7 @@ AS SELECT * FROM smartphones
 
 -- COMMAND ----------
 
+--The code selects all columns from the temporary view global_temp_view_latest_phones in the global temporary database.
 SELECT * FROM global_temp.global_temp_view_latest_phones;
 
 -- COMMAND ----------
