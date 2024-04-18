8091fe9320bcb9a13b7a70e20517d47cfaffe208
Luis-Martinez1 <Luis-Martinez1>
2024-04-18 16:15:20 +0000

    added comments

--- 1.1_Notebook_Basics.py
+++ 1.1_Notebook_Basics.py
@@ -49,28 +49,33 @@ print(full_name)
 
 # COMMAND ----------
 
-# MAGIC %fs ls '/databricks-datasets'
+# This code is listing the contents of the directory '/databricks-datasets'.
+%fs ls '/databricks-datasets'
 
 # COMMAND ----------
 
+
+#The code is calling the help() function on the dbutils object. This function will display information and documentation about how to use the functionality provided by dbutils.
 dbutils.help()
 
 # COMMAND ----------
 
+#The code dbutils.fs.help() is using the dbutils module in Databricks to access file system operations. fs.help() is a method that displays the help documentation for the file system operations available in Databricks.
 dbutils.fs.help()
 
 # COMMAND ----------
 
+# The code is using the dbutils.fs.ls() function to list all the files and directories in the '/databricks-datasets' directory. It then prints out the list of files and directories retrieved.
 files = dbutils.fs.ls('/databricks-datasets')
 print(files)
 
 # COMMAND ----------
 
+#The code is using the display function to show the contents of the files variable. It is assumed that files is a collection of some sort (like a list or an array) that contains file names. The purpose of this code is to display the file names to the user.
 display(files)
 
 # COMMAND ----------
 
-#can you help me 
 print ("hello data bricks")
 
 # COMMAND ----------
