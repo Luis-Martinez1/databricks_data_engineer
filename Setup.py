e6460cd4700204eef65826c6480d1286c990359b
Luis-Martinez1 <Luis-Martinez1>
2024-04-21 17:48:44 +0000

    added setup

---
+++ Setup.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+#This Python code consists of a single line where a variable named full_name is defined and assigned the string value "Derar Alhussein". Essentially, you're storing the name Derar Alhussein in a variable that can be used later in the code for various purposes, such as printing it out, including it in a database entry, or manipulating it in any way needed.
+
+full_name = "Derar Alhussein"
